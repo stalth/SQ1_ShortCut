64b69dcaee702fabcbd137d67a0c7f41542c677a
stalth <44404140+stalth@users.noreply.github.com>
2023-08-09 14:58:14 +0200

    function for random failure specifically on edge-disjoint path

--- SQ1.py
+++ SQ1.py
@@ -39,16 +39,16 @@ def create_graphs(n, k, rep, seed):
     return g 
 
 #Create a list of edge-disjoint paths to destination, source nodes vary
-def get_disjoint_path(g, d):
+def get_disjoint_path(g, destination):
     SQ1 = {}
     H = build_auxiliary_edge_connectivity(g)
     R = build_residual_network(H, 'capacity')
     SQ1 = {n: {} for n in g}
     for u in g.nodes():
-        if (u != d):
+        if (u != destination):
             k = sorted(list(nx.edge_disjoint_paths(
-                g, u, d, auxiliary=H, residual=R)), key=len)
-            SQ1[u][d] = k
+                g, u, destination, auxiliary=H, residual=R)), key=len)
+            SQ1[u][destination] = k
     return SQ1
 
 def routingSQ1(source, destination, fails, shortCut):
@@ -56,13 +56,18 @@ def routingSQ1(source, destination, fails, shortCut):
 
 #Create a list of fails, either random or specifically on edge-disjoint paths
 def get_fails_list(g, source, destination, disjoint_path_list, fail_random, failure_num):
-    edge_list = g.edges()
+    edge_list = list(g.edges())
+    num_edgedesjointpaths = len(disjoint_path_list[source][destination])
     fails_list = []
     if fail_random:
         for i in range(failure_num):
             fails_list.append(random.choice(edge_list))
     else:
-        alla = 3
+        for i in range(num_edgedesjointpaths):
+            nodeindex_in_path = random.randint(0, len(disjoint_path_list[source][destination][i])-1)
+            while nodeindex_in_path == len(disjoint_path_list[source][destination][i])-1:
+                nodeindex_in_path = random.randint(0, len(disjoint_path_list[source][destination][i])-1)
+            fails_list.append((disjoint_path_list[source][destination][i][nodeindex_in_path], disjoint_path_list[source][destination][i][nodeindex_in_path+1]))
     return fails_list
 
 #Initialize parameters for experiments, take runtime, start experiments
@@ -76,7 +81,7 @@ if __name__ == "__main__":
     k = 8
     failure_num = 40
     ran_dom = True
-    fail_random = True
+    fail_random = False
     #G = nx.Graph()
     #G.add_edges_from([(1, 4), (4, 5), (1, 2), (2,3), (3,5), (3,6), (6,5), (2,4)])
     
