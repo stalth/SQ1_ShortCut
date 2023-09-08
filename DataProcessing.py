481187ccc545e71b46c48e9fa50e47d016135a50
stalth <44404140+stalth@users.noreply.github.com>
2023-09-08 11:24:29 +0200

    DataProcessing collected for all source destination

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,7 +1,6 @@
 from obj_class_pickle import DataGraphs
 import matplotlib.pyplot as plt 
 import os
-import random
 from itertools import permutations
 
 def loadAndCalculateRatio(directory):
@@ -45,30 +44,64 @@ def draw(figdirectory, data2d, source, destination):
     if not os.path.exists(figdirectory):
         os.makedirs(figdirectory)
     fig = plt.figure()
-    # TODO: Öffnet grade Diagramme anstatt einfach nur dateien zu schreiben
     plt.ioff()
     plt.boxplot(data2d)
-    plt.title("Boxplot Using Matplotlib")
+    #plt.title("Boxplot Using Matplotlib")
     plt.xlabel('number of failures')
+    plt.ylabel('hops ratio without/with ShortCut')
     fig.savefig(f"{figdirectory}/{source}_{destination}.png", dpi=fig.dpi)
     plt.close(fig)
 
+def draw_collected(figdirectory, data2d):
+    if not os.path.exists(figdirectory):
+        os.makedirs(figdirectory)
+    fig = plt.figure()
+    plt.ioff()
+    plt.boxplot(data2d)
+    #plt.title("Boxplot Using Matplotlib")
+    plt.xlabel('number of failures')
+    plt.ylabel('hops ratio without/with ShortCut')
+    fig.savefig(f"{figdirectory}/collected.png", dpi=fig.dpi)
+    plt.close(fig)
+
+def fillDict_collected(resultRatiosByFile):
+    preparedDict = prepareResultDict_collected(resultRatiosByFile)
+    for filename in resultRatiosByFile:
+        for source in resultRatiosByFile[filename]:
+            for destination in resultRatiosByFile[filename][source]:
+                lst_specified = resultRatiosByFile[filename][source][destination] 
+                for j in range(0, len(lst_specified)):
+                    preparedDict[j].append(lst_specified[j])
+    return preparedDict
+
+def prepareResultDict_collected(resultRatiosByFile):
+    result_dict_by_failureNum = {}
+    for firstkey in resultRatiosByFile:
+        for src2 in resultRatiosByFile[firstkey]:
+            #result_dict_by_failureNum[src2] = {}
+            for destination2 in resultRatiosByFile[firstkey][src2]:
+                #result_dict_by_failureNum[src2][destination2] = {}
+                for i in range(0, len(resultRatiosByFile[firstkey][src2][destination2])):  # noqa: E501
+                    result_dict_by_failureNum[i] = []
+    return result_dict_by_failureNum
+
 folders = ["30nodes","40nodes","50nodes","60nodes","70nodes","80nodes","90nodes","100nodes"]  # noqa: E501
 for folder in folders:
-
     subfolder = folder
     directory = f"C:/Repositories/SQ1_ShortCut/results/{subfolder}"
     Figdirectory = f"C:/Repositories/SQ1_ShortCut/figs/{subfolder}"
     resultRatiosByFile, nodes = loadAndCalculateRatio(directory)
-    endResult = fillDict(resultRatiosByFile)
+    #endResult = fillDict(resultRatiosByFile)
+    endResult = fillDict_collected(resultRatiosByFile)
 
-    lst_nodes = list(range(0, nodes))
-    perm_nodes = permutations(lst_nodes, 2)
-    for perm in perm_nodes:
-        #print('Start: ' + str(perm[0]) + ' Ziel: ' + str(perm[1]))
-        source = perm[0]
-        destination = perm[1]
-        pass
-        data2d = list(endResult[source][destination].values())[1:]
-        draw(Figdirectory, data2d, source, destination)
-        pass
+    #lst_nodes = list(range(0, nodes))
+    #perm_nodes = permutations(lst_nodes, 2)
+    data2d = list(endResult.values())[1:]
+    draw_collected(Figdirectory, data2d)
+    pass
+    #for perm in perm_nodes:
+        #source = perm[0]
+        #destination = perm[1]
+        #data2d = list(endResult[source][destination].values())[1:]
+        #draw(Figdirectory, data2d, source, destination)
+        #pass
